24e6975c072f01f968554fc7718914f52d9aa96e
dani limbeck <dani.limbeck@gmx.at>
2023-07-21 21:33:44 +0200

    ad list of ships (user) and prints board again with ships

--- battleships.py
+++ battleships.py
@@ -5,13 +5,14 @@ def create_abc():
         abc[i] = alphabet[i]
     return abc
 
-def set_board():
+def init_board():
+    #creates the board-dict
     y_numbers = 5
     board = []
     for i in range(y_numbers):
         board.append(["."]*y_numbers)
 
-
+    #prints the board
     abc = create_abc()
     row = 0
     print(" ", "1", "2", "3", "4", "5") #unschoen
@@ -23,13 +24,38 @@ def set_board():
 
 
 def set_ships_user():
-    board = set_board()
+    abc = {}
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(26):
+        abc[alphabet[i]] = i 
+    board = init_board()
     print(board)
+    ships_user = []
     for i in range(2):
         #user input of ship location
-        while True:
-            try:
-                y = int(input("enter row of ship: "))-1
+        y = abc[input("enter row of ship: ")]
+        x = int(input("enter column of ship: "))
+        ships_user.append([y,x])
+        print(ships_user)
+        if board[y-1][x-1] == ".":
+            board[y-1][x-1] = "o"
+        elif board[y][x] == "o":
+            print("already taken")
+    print(board)
+    #abc = create_abc()
+    
+    
+    row = 0
+    print(" ", "1", "2", "3", "4", "5") #unschoen
+    for line in board:
+        print([key for key in abc.keys()][row], "|".join(line))
+        row +=1
+        
+       
+    return board, ships_user
+    
+
+'''
                 if y < len(board):
                     print("ok")
                     break
@@ -39,23 +65,11 @@ def set_ships_user():
             except:
                 print("Provide an integer value...")
                 continue
-
-                x = int(input("enter column of ship: "))-1
+'''
 
                 #check if empty
-                if board[y][x] == ".":
-                    board[y][x] = "o"
-                elif board[y][x] == "o":
-                    print("already taken")
-
-    abc = create_abc()
-    row = 0
-    print(" ", "1", "2", "3", "4", "5") #unschoen
-    for line in board:
-        print(abc[row], "|".join(line))
-        row +=1
-        
-    return board
+                
+    
 
 def set_ships_pc():
     board = set_board()
