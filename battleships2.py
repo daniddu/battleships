66bfb0942b7ae74e24f1e2919c1dbc69a1c868c4
dani limbeck <dani.limbeck@gmx.at>
2023-08-23 23:41:08 +0200

    check if ship sunken (and hitship = 0) doesnt work

--- battleships2.py
+++ battleships2.py
@@ -2,20 +2,19 @@ import random
 
 def create_abc_user():
     abc = {}
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for i in range(26):
+    alphabet = "ABCDEFGHIJ"
+    for i in range(10):
         abc[alphabet[i]] = i
     return abc
 
 def create_abc_pc():
     abc = {}
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for i in range(26):
+    alphabet = "ABCDEFGHIJ"
+    for i in range(10):
         abc[i] = alphabet[i]
     return abc
 
 def init_board(size):
-    size = 10 #make it input?
     board = []
     for i in range(size):
         board.append(["."]*size)
@@ -41,18 +40,16 @@ def print_board_pc(board_pc, abc, size):
         print(abc[row], "|".join(line))
         row +=1
 
-def set_ships_user(length,board_user,ships_user):
+def set_ships_user(length,board_user,ships_user,size):
     abc = create_abc_user() 
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    size = 10 #size-input fehlt
+    alphabet = "ABCDEFGHIJ"
     ship = []
 
     while True:
         y_start = input("enter row of ships starting point: ")
 
-        if not y_start.upper() in alphabet:
-            print("please enter a character between A and J.")
-            continue
+        while not y_start.upper() in alphabet:
+            y_start = input("please enter a character between A and J: ")
         y = int(abc[y_start.upper()])
 
         if y > size:
@@ -71,10 +68,8 @@ def set_ships_user(length,board_user,ships_user):
                     print("please enter a number.")
 
         hor_ver = input("horizontal (H) or vertical (V)? ")
-
-        if hor_ver.upper() not in ['H', 'V']:
-            print("please enter H or V.")
-            #continue
+        while not hor_ver.upper() in ['H', 'V']:
+            hor_ver = input("please enter H or V:")
 
         if hor_ver.upper() == "H":
             if x-1+length >= size:
@@ -116,12 +111,13 @@ def set_ships_user(length,board_user,ships_user):
             for i in range(y,(y+length)):
                 board_user[i][x] = "o"
                 ship.append([i,x])   
+        else:
+            continue
 
         return board_user, ship, abc
 
-def set_ships_pc(length,board_pc,ships_pc):
+def set_ships_pc(length,board_pc,ships_pc,size):
     abc = create_abc_pc()
-    size = 10
     ship = []
   
     while True:
@@ -179,30 +175,27 @@ def set_ships(ships_user, ships_pc, board_user, board_pc, size):
     ships = [5,3] #533222
     for i in ships:
         print("set ship with length {}:".format(i))
-
-        board_user,ship,abc = set_ships_user(i,board_user,ships_user)
+        board_user,ship,abc = set_ships_user(i,board_user,ships_user,size)
         ships_user.append(ship)
         print("\n"+"your placed ships:"+"\n")
         print_board_user(board_user,abc,size)
         print("\n")
-
-        board_pc,ship_pc,abc = set_ships_pc(i,board_pc,ships_pc)
+        board_pc,ship_pc,abc = set_ships_pc(i,board_pc,ships_pc,size)
         ships_pc.append(ship_pc)
-        #print_board_pc(board_pc,abc,size) #entfernen
 
     return board_user, board_pc
 
 #setting board and placing ships
 def set_board():
-    size = int(input("size: "))
+    size = 10
     board_user = init_board(size)
     abc = create_abc_user()
+    print("you have to place 6 ships with lengths 5 (1x), 3 (2x) and 2 (3x) on this board.", "\n")
     print_board_user(board_user, abc, size)
     board_pc = init_board(size)
     print("")
     ships_user = []
     ships_pc = []
-    print("you have to place 6 ships with length 5 (1x), 3 (2x) and 2 (3x).")
     set_ships(ships_user, ships_pc, board_user, board_pc, size)
     return board_user, board_pc, ships_user, ships_pc, size
 
@@ -210,9 +203,9 @@ def set_board():
 def user_guess(board_pc, ships_pc, guessing_board, size):
     ships = [5,3]
     abc = create_abc_user() 
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alphabet = "ABCDEFGHIJ"
     while True:
-        row_num = input("guess y: ")
+        row_num = input("guess row of the ship: ")
         if not row_num.upper() in alphabet:
             print("please enter a character between A and J")
             continue
@@ -225,7 +218,7 @@ def user_guess(board_pc, ships_pc, guessing_board, size):
 
         while True:
             try:
-                x = int(input("guess x: "))-1
+                x = int(input("guess column of the ship: "))-1
                 if x >= size or x < 0:
                     print("out of range")
                     continue
@@ -233,70 +226,190 @@ def user_guess(board_pc, ships_pc, guessing_board, size):
                     break
             except ValueError:
                 print("please enter a number")
-
                               
         if guessing_board[y][x] == "/" or guessing_board[y][x] == "X":
             print("already guessed")
         if board_pc[y][x] == "o":
             guessing_board[y][x] = "X"
             board_pc[y][x] = "X"
+            print("hit!")
 
             for i in range(len(ships_pc)): 
                 for o in range(len(ships_pc[i])):
                     if ships_pc[i][o-1] == [y,x]:
-                        print(i)
                         print("size of target:",ships[i])
-                        print(ships_pc[i][o])
                         ships_pc[i].remove([y,x])
         
         elif board_pc[y][x] == ".":
             guessing_board[y][x] = "/"
             print("nope!")
     
-        #print(ships_pc.count([]))
-        print("{} of pcs ships sunken.".format(ships_pc.count([]))) #how many left?
+        print("{} of pcs ships sunken.".format(ships_pc.count([])))
         return board_pc, ships_pc, guessing_board
 
-def pc_guess(board_user, ships_user, size):
-    ships = [5,3]
-    y = random.randrange(0, size)
-    x = random.randrange(0, size)
+def pc_guess(board_user, ships_user, size, hitship):
+    ships = (5,3)
 
-    if board_user[y][x] == "/":
-        print("pc, already guessed")
-    if board_user[y][x] == "o":
-        board_user[y][x] = "X"
-        for i in range(len(ships_user)):
-            for o in range(i):
-                if ships_user[i][o] == [y,x]:
-                    print(i)
-                    print("size of targ:",ships[i])
-                    print(ships_user[i][o])
-                    ships_user[i].remove([y,x])
-        
-    elif board_user[y][x] == ".":
-        board_user[y][x] = "/"
-        print("not hit!")
+    if hitship == 0:
+        y = random.randrange(0, size)
+        x = random.randrange(0, size)
 
-    print("{} of users ships sunken!".format(ships_user.count([])))
+        if board_user[y][x] == "/":
+            pass
+        elif board_user[y][x] == "o":
+            board_user[y][x] = "X"
+            for i in ships_user:
+                print(ships_user)
+                pos = ships_user.index(i)
+                for o in i:
+                    pos2 = ships_user[pos].index(o)
+                    if ships_user[pos][pos2] != [y,x]:
+                        pass
+                    elif ships_user[pos][pos2] == [y,x]:
+                        #p = i
+                        print(i)
+                        print("size of target:",ships[pos])
+                        print("y",ships_user[pos][pos2])
+                        hitship = ships_user[pos][pos2]
+                        ships_user[pos].remove([y,x])
 
-    return board_user, ships_user
+        elif board_user[y][x] == ".":
+            board_user[y][x] = "/"
+            print("not hit!")
+            hitship = 0
 
-def guessing(board_user, board_pc, ships_user, ships_pc, size):
+        print("{} of users ships sunken!".format(ships_user.count([])))
+    
+    elif hitship != 0:
+        while True:
+            print("x",hitship)
+            y = int(hitship[0])
+            x = int(hitship[1])
+
+                    
+            if board_user[y][x+1] == "o":
+                board_user[y][x+1] = "X"
+                hitship = ([y,x+1])
+                print("hit!")
+                break 
+            elif board_user[y][x+1] == ".":
+                board_user[y][x+1] = "/"
+                print("not hit!")
+                break
+            elif board_user[y][x+1] == "X":
+                pass
+            elif board_user[y][x+1] == "/" or ((x+1) > 9):
+                i = 0
+                while board_user[y][x-i] == "X" and (x+i) <= 9:
+                    i += 1
+                    print("n",board_user[y][x-i])
+                    #if (x+i) > 9:
+                        #continue
+                else:
+                    board_user[y][x-i] = "X"
+                    hitship = ([y,x-i])
+                    break
+
+            if board_user[y][x-1] == "o":
+                board_user[y][x-1] = "X"
+                hitship = ([y,x-1])
+                print("hit!")
+                break
+            elif board_user[y][x-1] == ".":
+                board_user[y][x-1] = "/"
+                print("not hit!")
+                break
+            elif board_user[y][x-1] == "X":
+                pass
+            elif board_user[y][x-1] == "/" or ((x-1) < 0):
+                i = 1
+                while board_user[y][x+i] == "X" and (x-i) >= 0:
+                    i += 1
+                    if (x-i) < 0:
+                        pass
+                else:
+                    board_user[y][x+i] = "X"
+                    hitship = ([y,x+i])
+                    break
+            
+            if board_user[y-1][x] == "o":
+                board_user[y-1][x] = "X"
+                hitship = ([y-1,x])
+                print("hit!")
+                break
+            elif board_user[y-1][x] == ".":
+                board_user[y-1][x] = "/"
+                print("not hit!", board_user[y-1][x])
+                break
+            elif board_user[y-1][x] == "X":
+                pass                
+            elif board_user[y-1][x] == "/" or ((y-1) < 0):
+                i = 1
+                while board_user[y+i][x] == "X" and (y-i) >= 0:
+                    i += 1
+                    if (y-i) < 0:
+                        pass
+                else:
+                    board_user[y+i][x] = "X"
+                    hitship = ([y+1,x])
+                    break
+            
+            if board_user[y+1][x] == "o":
+                board_user[y+1][x] = "X"
+                hitship = ([y+1,x])
+                print("hit!")
+                break
+            elif board_user[y+1][x] == ".":
+                board_user[y+1][x] = "/"
+                print("not hit!", board_user[y+1][x])
+                break
+            elif board_user[y+1][x] == "X":
+                pass                
+            elif board_user[y+1][x] == "/" or ((y+1) > 9):
+                i = 1
+                while board_user[y-i][x] == "X" and (y+i) <= 9:
+                    i += 1
+                    if (y+i) > 9:
+                        pass
+                else:
+                    board_user[y-i][x] = "X"
+                    print("hit!")
+                    hitship = ([y-1,x])
+                    break
+                      
+            for i in ships_user:
+                pos = ships_user.index(i)
+                for o in i:
+                    pos2 = ships_user[pos].index(o)
+                    if ships_user[pos][pos2] != hitship:
+                        pass
+                    elif ships_user[pos][pos2] == hitship:
+                        ships_user[pos].remove(hitship)
+                        #break
+                    print("w",ships_user)  
+                
+                if len(ships_user[pos]) == 0:
+                    print(ships_user[pos])
+                    hitship = 0
+
+    return board_user, ships_user, hitship
+
+def guessing(board_user, board_pc, ships_user, ships_pc, size, hitship):
     abc = create_abc_pc()
+    #abc = create_abc_user
     guessing_board = init_board(size)
 
     while len(ships_user) != ships_user.count([]) and len(ships_pc) != ships_pc.count([]):
         print("it's your turn:")
         board_pc, ships_pc, guessing_board = user_guess(board_pc, ships_pc, guessing_board, size)
         #print(guessing_board)
-        #print(ships_pc)
         print("\n"+"your guessing board:"+"\n")
         print_board_pc(guessing_board, abc, size)
         print("pcs turn:")
-        board_user, ships_user = pc_guess(board_user, ships_user, size)
+        board_user, ships_user, hitship = pc_guess(board_user, ships_user, size, hitship)
+        print("b",hitship)
     
-    return board_pc, ships_pc, guessing_board, board_user, ships_user
+    return board_pc, ships_pc, guessing_board, board_user, ships_user, hitship
    
 def play_battleship():
     print("\n"+"========== lets play battleships! ==========", "\n")
@@ -305,17 +418,19 @@ def play_battleship():
     print(" "*2+"_______/_____\_______\_____")
     print(" "*2+"\\"+" "*14+"< "*3+" "*6+"/")
     print("~"*32, "\n")
-
+    input("press enter to start the game.")
+    print("\n")
     board_user, board_pc, ships_user, ships_pc, size = set_board()
-    print("\n"+"lets start guessing!"+"\n")
-    board_pc, ships_pc, guessing_board, board_user, ships_user = guessing(board_user, board_pc, ships_user, ships_pc, size)
-    print(len(ships_pc))
-    print("x", ships_pc)
+    print("lets start guessing!"+"\n")
+    hitship = 0
+    board_pc, ships_pc, guessing_board, board_user, ships_user = guessing(board_user, board_pc, ships_user, ships_pc, size, hitship)
+    #print(len(ships_pc))
+    #print("x", ships_pc)
     if len(ships_user) == ships_user.count([]):
         print(len(ships_user),ships_user.count([]))
         print("pc is the winner!") 
     elif len(ships_pc) == ships_pc.count([]):
-        print(len(ships_pc),ships_pc.count([]))
+        #print(len(ships_pc),ships_pc.count([]))
         print("you are the winner!")
        
 
